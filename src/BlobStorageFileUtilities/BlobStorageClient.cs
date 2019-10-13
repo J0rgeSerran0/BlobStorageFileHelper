c261efb0f221e1e27eea8f8651ec7a5592df5208
Jorge Serrano Pérez <jorge.serrano.perez@gmail.com>
2019-10-13 16:51:56 +0200

    Process simplified

--- src/BlobStorageFileUtilities/BlobStorageClient.cs
+++ src/BlobStorageFileUtilities/BlobStorageClient.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Azure.Storage;
 using Microsoft.Azure.Storage.Blob;
 using System;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace BlobStorageFileUtilities
@@ -8,10 +9,16 @@ namespace BlobStorageFileUtilities
     public class BlobStorageClient
     {
         private readonly CloudBlobClient _cloudBlobClient = null;
+        private readonly CloudBlobContainer _blobStorageContainer = null;
 
-        public BlobStorageClient(string blobStorageConnectionString)
+        private readonly string _container = null;
+
+        public string ErrorDetails = String.Empty;
+
+        public BlobStorageClient(string blobStorageConnectionString, string container)
         {
             if (String.IsNullOrEmpty(blobStorageConnectionString)) throw new ArgumentException($"{nameof(blobStorageConnectionString)} is null or empty");
+            if (String.IsNullOrEmpty(container)) throw new ArgumentException($"{nameof(container)} is null or empty");
 
             CloudStorageAccount storageAccount;
 
@@ -23,13 +30,56 @@ namespace BlobStorageFileUtilities
             {
                 throw new Exception("The connection string is not valid. An error has been returned. Check it and try again.");
             }
+
+            _container = container;
+            _blobStorageContainer = GetContainerReference(_container);
         }
 
-        public async Task<CloudBlobContainer> GetContainerReferenceAsync(string container)
+        private CloudBlobContainer GetContainerReference(string container)
         {
             if (String.IsNullOrEmpty(container)) throw new ArgumentException($"{nameof(container)} is null or empty");
 
-            return await Task.Run(() => _cloudBlobClient.GetContainerReference(container));
+            return _cloudBlobClient.GetContainerReference(container);
+        }
+
+        public async Task<Stream> DownloadAsync(string filename)
+        {
+            try
+            {
+                Stream stream = new MemoryStream();
+
+                var cloudBlockBlob = _blobStorageContainer.GetBlockBlobReference(filename);
+                
+                await cloudBlockBlob.DownloadToStreamAsync(stream);
+
+                return stream;
+            }
+            catch (Exception ex)
+            {
+                ErrorDetails = ex.Message;
+
+                return null;
+            }
+        }
+
+        public async Task<bool> UploadAsync(string filename, Stream stream)
+        {
+            try
+            {
+                ErrorDetails = String.Empty;
+
+                var cloudBlockBlob = _blobStorageContainer.GetBlockBlobReference(filename);
+
+                await cloudBlockBlob.UploadFromStreamAsync(stream);
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                ErrorDetails = ex.Message;
+
+                return false;
+            }
         }
 
     }
